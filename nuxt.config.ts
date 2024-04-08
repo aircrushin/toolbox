5d07c0f7801586d46a809fd34394613b12d276b8
aircrushin <88492452+aircrushin@users.noreply.github.com>
2024-04-08 20:12:49 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+    app:{
+    head: {
+      title: 'Toolbox',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: "baidu-site-verification", content:"codeva-lL8vNDClGc" },
+      ],
+      link: [
+        { rel: 'icon', type:'ico/svg', href:'/toolbox.ico'}
+      ]
+    },
+  },
   modules: [
     '@vueuse/nuxt',
     '@pinia/nuxt',
