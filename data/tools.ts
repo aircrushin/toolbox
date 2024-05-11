309620c3d1fd74c184ea9e65f32171303c51066f
aircrushin <88492452+aircrushin@users.noreply.github.com>
2024-05-11 10:24:47 +0800

    Update tools.ts

--- data/tools.ts
+++ data/tools.ts
@@ -1,52 +1,22 @@
 export const tools = [
     {
-        title: "breditor",
-        desc: "Markdown转图片",
-        link: 'https://breditor.vercel.app/'
-    },
-    {
-        title: "BPM",
-        desc: "BPM和调性查询",
-        link: 'https://vocalremover.org/zh/key-bpm-finder'
-    },
-    {
-        title: "Auto-Attack",
-        desc: "查询压缩器的启动和释放时间",
-        link: 'https://auto-attack.zeabur.app/'
+        title: "Youtube",
+        desc: "Most popular video website",
+        link: 'https://www.youtube.com/'
     },
     {
         title: "Kimi Chat",
-        desc: "支持联网和上传文件的Chatbot",
+        desc: "A chatbot support online search and file upload",
         link: 'https://kimi.moonshot.cn/'
     },
     {
-        title: "remove-bg",
-        desc: "在线去除背景图",
-        link: 'https://www.remove.bg/zh'
-    },
-    {
         title: "air-todo",
-        desc: "极简TodoList",
+        desc: "Simple todoList",
         link: 'https://airtodo.zeabur.app/'
     },
     {
-        title: "Indie dev",
-        desc: "独立开发者学习文档",
-        link: 'https://indie.npmstudy.com/'
-    },
-    {
-        title: "Landr",
-        desc: "音乐采样网站",
-        link: 'https://www.landr.com/'
-    },
-    {
-        title: "智谱开放平台",
-        desc: "AI项目开发者的api工具",
-        link: 'https://open.bigmodel.cn/usercenter/apikeys'
-    },
-    {
         title: "v0.dev",
-        desc: "React+tailwind样式生成器",
+        desc: "React+tailwind stylesheet generator",
         link: 'https://v0.dev/'
     },
 ]
